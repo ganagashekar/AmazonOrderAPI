5d36e2a7b903048e00aa67e67d815edeaf6806cb
ganagashekar <ganga123081@gmail.com>
2023-06-22 12:06:48 +0530

    Update CustomClaimTypes.cs

--- AmazonOrderAPI/Configuration/CustomClaimTypes.cs
+++ AmazonOrderAPI/Configuration/CustomClaimTypes.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+ var output = new Dictionary<string, string>();
 
-namespace AmazonOrderAPI.Configuration
-{
-    public static class CustomClaimTypes
-    {
-        public const string SellerId = "SellerId";
-    }
-}
+            var csFilePath = @"C:\Users\Haadv\source\repos\ConsoleApp6\TestProgram.cs";
+            var csFileContent = File.ReadAllText(csFilePath);
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(csFileContent);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+            var nds = (NamespaceDeclarationSyntax)root.Members[0];
+            var cds = (ClassDeclarationSyntax)nds.Members[0];
+
+            foreach (var ds in cds.Members)
+            {
+                //Only take methods into consideration
+                if (ds is MethodDeclarationSyntax)
+                {
+                    var mds = (MethodDeclarationSyntax)ds;
+
+                    //Method name
+                    var methodName = ((SyntaxToken)mds.Identifier).ValueText;
+
+                    //Method body (including curly braces)
+                    var methodBody = mds.Body.ToString();
+
+                    output.Add(methodName, methodBody);
+                }
+            }
